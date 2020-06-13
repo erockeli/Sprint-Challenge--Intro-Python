40333f5bfe4f5320dcccdb02c8b0e4daea55cfe0
Eric Foster <elitheroyalone@gmail.comgit config --global user.name eric-fostergit config --global user.email elitheroyalone@gmail.com>
2020-06-13 13:08:40 -0500

    Done ,but let check over work

--- src/cityreader/cityreader.py
+++ src/cityreader/cityreader.py
@@ -1,5 +1,15 @@
+import csv
 # Create a class to hold a city location. Call the class "City". It should have
 # fields for name, lat and lon (representing latitude and longitude).
+
+class City:
+  def __init__(self,name, lat,lon):
+    self.name = name
+    self.lat = lat
+    self.lon = lon
+    
+  def __str__(self):
+    return (f"{self.name} {self.lat} {self.lon}\n")
 
 
 # We have a collection of US cities with population over 750,000 stored in the
@@ -20,6 +30,16 @@ def cityreader(cities=[]):
   # TODO Implement the functionality to read from the 'cities.csv' file
   # For each city record, create a new City instance and add it to the 
   # `cities` list
+
+   with open('cities.csv', newline='') as csvfile:
+    citiesreader = csv.reader(csvfile,delimiter=',', quotechar='|')
+    for row in citiesreader:
+      name = ''.join(row[0])
+      lat = ''.join(row[3])
+      lon = ''.join(row[4])
+      
+      
+      cities.append(City(name,lat,lon))
     
     return cities
 
