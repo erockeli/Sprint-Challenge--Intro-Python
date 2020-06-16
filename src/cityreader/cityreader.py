874d7d7e6e845cf91a0866f0ecccd8e2ea0e18cc
Eric Foster <elitheroyalone@gmail.comgit config --global user.name eric-fostergit config --global user.email elitheroyalone@gmail.com>
2020-06-16 14:06:15 -0500

    update

--- src/cityreader/cityreader.py
+++ src/cityreader/cityreader.py
@@ -1,21 +1,30 @@
-import csv
 # Create a class to hold a city location. Call the class "City". It should have
 # fields for name, lat and lon (representing latitude and longitude).
 
-class City:
-  def __init__(self,name, lat,lon):
-    self.name = name
-    self.lat = lat
-    self.lon = lon
-    
-  def __str__(self):
-    return (f"{self.name} {self.lat} {self.lon}\n")
+import csv
 
+
+class City:
+    def __init__(self, name, lat=0, lon=0):
+        self.name = name
+        self.lat = lat
+        self.lon = lon
+
+    def __str__(self):
+        return f"{self.name}: {self.lat}, {self.lon}"
+
+    def __repr__(self):
+        return f"{self.name}: {self.lat}, {self.lon}"
+
+
+# new_city = City("Whateverville", 4234242, -2432251)
+
+# print(type(new_city.lat))
 
 # We have a collection of US cities with population over 750,000 stored in the
 # file "cities.csv". (CSV stands for "comma-separated values".)
 #
-# In the body of the `cityreader` function, use Python's built-in "csv" module 
+# In the body of the `cityreader` function, use Python's built-in "csv" module
 # to read this file so that each record is imported into a City instance. Then
 # return the list with all the City instances from the function.
 # Google "python 3 csv" for references and use your Google-fu for other examples.
@@ -24,30 +33,59 @@ class City:
 #
 # Note that the first line of the CSV is header that describes the fields--this
 # should not be loaded into a City object.
+
+# Understand
+#   In `cityreader` func use built-in "csv" module to READ file
+#   return the city list w/ all the City instances from the func
+#   So for each city record create a new City and add it to the city list
+#   First row in CSV file should be ignored when making the list
+
+
+# Plan
+#   import csv, look at python 3 csv for references
+
+# Usage Example
+# >>> import csv
+# >>> with open('eggs.csv', newline='') as csvfile:
+# ...     spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
+# ...     for row in spamreader:
+# ...         print(', '.join(row))
+
+# with open('./cities.csv, newline='') as csvfile:
+#   csv_reader = csv.reader(csvfile)
+#     for item in csvfile:
+#       cities.append(City(item[0], item[3], item[4]))
+
+
 cities = []
 
-def cityreader(cities=[]):
-  # TODO Implement the functionality to read from the 'cities.csv' file
-  # For each city record, create a new City instance and add it to the 
-  # `cities` list
+# Reflect
 
-   with open('cities.csv', newline='') as csvfile:
-    citiesreader = csv.reader(csvfile,delimiter=',', quotechar='|')
-    for row in citiesreader:
-      name = ''.join(row[0])
-      lat = ''.join(row[3])
-      lon = ''.join(row[4])
-      
-      
-      cities.append(City(name,lat,lon))
-    
+
+# Execute
+def cityreader(cities=[]):
+    #     # TODO Implement the functionality to read from the 'cities.csv' file
+    #     # For each city record, create a new City instance and add it to the
+    #     # `cities` list
+    with open('cities.csv', 'r') as csvfile:  
+        csv_file = csv.reader(csvfile)
+
+        for row in csv_file:
+            if row[0] != "city":  
+                city = City(row[0], float(row[3]), float(
+                    row[4]))  
+                cities.append(city)
+
     return cities
+
 
 cityreader(cities)
 
-# Print the list of cities (name, lat, lon), 1 record per line.
+
 for c in cities:
     print(c)
+
+
 
 # STRETCH GOAL!
 #
